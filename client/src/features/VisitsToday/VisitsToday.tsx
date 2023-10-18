04a9183bd24d034c277714089b5d760ff9dd6d04
artemenkoS <artemenko@ya.ru>
2023-10-18 16:25:08 +0500

    update dockerfile

--- client/src/features/VisitsToday/VisitsToday.tsx
+++ client/src/features/VisitsToday/VisitsToday.tsx
@@ -1,25 +1,38 @@
-import EditIcon from '@mui/icons-material/Edit';
-import { CircularProgress, IconButton, Pagination, Typography } from '@mui/material';
-import format from 'date-fns/format';
-import { ru } from 'date-fns/locale';
-import React from 'react';
-import toast from 'react-hot-toast';
+import EditIcon from "@mui/icons-material/Edit";
+import {
+  CircularProgress,
+  IconButton,
+  Pagination,
+  Typography,
+} from "@mui/material";
+import format from "date-fns/format";
+import { ru } from "date-fns/locale";
+import React from "react";
+import toast from "react-hot-toast";
 
-import { useGetDoctorsQuery } from '../../api/doctor/doctorApi';
-import { useGetVisitsQuery } from '../../api/visit/visitApi';
-import { DeleteVisitButton } from '../../components/DeleteVisitModal/DeleteVisitButton';
-import { getTodayDate } from '../../helpers/getTodayDate';
-import { List, Slot, Wrapper } from './styled';
+import { useGetDoctorsQuery } from "../../api/doctor/doctorApi";
+import { useGetVisitsQuery } from "../../api/visit/visitApi";
+import { DeleteVisitButton } from "../../components/DeleteVisitModal/DeleteVisitButton";
+import { getTodayDate } from "../../helpers/getTodayDate";
+import { List, Slot, Wrapper } from "./styled";
 
 export const VisitsToday = () => {
   const todayDate = getTodayDate();
 
   const [page, setPage] = React.useState<number>();
 
-  const { data: visits, isLoading, isSuccess, isError } = useGetVisitsQuery({ startDate: todayDate, page: page });
+  const {
+    data: visits,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useGetVisitsQuery({ startDate: todayDate, page: page });
   const { data: doctors } = useGetDoctorsQuery();
 
-  const handlePaginationChange = (_: React.ChangeEvent<unknown>, value: number) => {
+  const handlePaginationChange = (
+    _: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
     setPage(value);
   };
 
@@ -28,7 +41,7 @@ export const VisitsToday = () => {
   }
 
   if (isError) {
-    toast('Не удалось загрузить список записей.');
+    toast("Не удалось загрузить список записей.");
   }
 
   return (
@@ -37,7 +50,10 @@ export const VisitsToday = () => {
         {isSuccess &&
           visits?.data.map((visit) => {
             const visitDate = new Date(visit.visitDate);
-            const formattedDate = format(visitDate, 'd MMMM HH:mm', { locale: ru });
+            console.log(visitDate);
+            const formattedDate = format(visitDate, "d MMMM HH:mm", {
+              locale: ru,
+            });
             const doctor = doctors?.data.find((el) => el.id === visit.doctorId);
             return (
               <Slot key={visit.id}>
@@ -52,7 +68,11 @@ export const VisitsToday = () => {
             );
           })}
       </List>
-      <Pagination count={visits?.pagination.totalPages} variant="outlined" onChange={handlePaginationChange} />
+      <Pagination
+        count={visits?.pagination.totalPages}
+        variant="outlined"
+        onChange={handlePaginationChange}
+      />
     </Wrapper>
   );
 };
