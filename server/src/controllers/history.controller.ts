28535b0e7bd968bba0da9b1f826d8071978d4625
artemenkoS <artemenko@ya.ru>
2023-10-24 00:59:08 +0500

    backend: add visits logging

--- server/src/controllers/history.controller.ts
+++ server/src/controllers/history.controller.ts
@@ -2,27 +2,26 @@ import { Request, Response } from "express";
 
 import db from "../db";
 
-export const createPatient = async (req: Request, res: Response) => {
-  const { name, surname, phone } = req.body;
+export const createLogRecord = async (req: Request, res: Response) => {
+  const { authorId, doctorId, changes, visitDate } = req.body;
 
-  if (!name || !surname || !phone) {
+  if (!authorId || !doctorId || !visitDate || !changes) {
     res.status(400).json("Не все обязательные поля заполнены");
     return;
   }
 
   try {
-    const newPatient = await db.query(
-      `INSERT INTO patient (name, surname, phone) values ($1, $2, $3) RETURNING *`,
-      [name, surname, phone]
+    const newLogRecord = await db.query(
+      `INSERT INTO history (authorId, doctorId, visitDate, changes) values ($1, $2, $3, $4) RETURNING *`,
+      [authorId, doctorId, visitDate, changes]
     );
 
     res.status(201).json({
-      patient: newPatient.rows[0],
-      message: "Пациент успешно создан.",
+      log: newLogRecord.rows[0],
     });
   } catch (error) {
-    console.error("Ошибка при создании пациента:", error);
-    res.status(500).json("Произошла ошибка при создании пациента");
+    console.error("Ошибка при записи в историю:", error);
+    res.status(500).json("Произошла ошибка при записи в историю");
   }
 };
 
@@ -49,60 +48,62 @@ export const getHistory = async (req: Request, res: Response) => {
       },
     });
   } catch (error) {
-    console.error("Произошла ошибка при получении истории из:", error);
-    res.status(500).json("Произошла ошибка при получении списка пациентов");
-  }
-};
-
-export const getOnePatient = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const user = await db.query(`SELECT * FROM patient where id = $1`, [id]);
-    if (!user.rows[0]) {
-      res.status(404).json({ message: "Пользователь не найден." });
-    } else {
-      res.status(200).json({ data: user.rows[0] });
-    }
-  } catch (error) {
-    console.error("Что-то пошло не так");
+    console.error("Произошла ошибка при получении истории изменений", error);
     res
       .status(500)
-      .json("Произошла ошибка при получении информации о пациенте");
+      .json({ message: "Произошла ошибка при получении истории изменений" });
   }
 };
 
-export const updatePatient = async (req: Request, res: Response) => {
-  const { name, surname, phone, id } = req.body;
+// export const getOnePatient = async (req: Request, res: Response) => {
+//   try {
+//     const id = req.params.id;
+//     const user = await db.query(`SELECT * FROM patient where id = $1`, [id]);
+//     if (!user.rows[0]) {
+//       res.status(404).json({ message: "Пользователь не найден." });
+//     } else {
+//       res.status(200).json({ data: user.rows[0] });
+//     }
+//   } catch (error) {
+//     console.error("Что-то пошло не так");
+//     res
+//       .status(500)
+//       .json("Произошла ошибка при получении информации о пациенте");
+//   }
+// };
 
-  try {
-    const newPatient = await db.query(
-      `UPDATE patient set name =  $1, surname = $2, phone= $3 where id = $4 RETURNING *`,
-      [name, surname, phone, id]
-    );
+// export const updatePatient = async (req: Request, res: Response) => {
+//   const { name, surname, phone, id } = req.body;
 
-    res.status(201).json({
-      patient: newPatient.rows[0],
-      message: "Данные паицента успешно изменены.",
-    });
-  } catch (error) {
-    console.error("Ошибка при создании пациента:", error);
-    res.status(500).json("Произошла ошибка при создании пациента");
-  }
-};
+//   try {
+//     const newPatient = await db.query(
+//       `UPDATE patient set name =  $1, surname = $2, phone= $3 where id = $4 RETURNING *`,
+//       [name, surname, phone, id]
+//     );
 
-export const deletePatient = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const user = await db.query(`DELETE FROM patient where id = $1`, [id]);
-    if (user.rowCount === 0) {
-      res.status(404).json({ message: "Пациент не найден." });
-    } else {
-      res.status(200).json({ message: "Пациент успешно удален" });
-    }
-  } catch (error) {
-    console.error("Что-то пошло не так");
-    res
-      .status(500)
-      .json("Произошла ошибка при получении информации о пациенте");
-  }
-};
+//     res.status(201).json({
+//       patient: newPatient.rows[0],
+//       message: "Данные паицента успешно изменены.",
+//     });
+//   } catch (error) {
+//     console.error("Ошибка при создании пациента:", error);
+//     res.status(500).json("Произошла ошибка при создании пациента");
+//   }
+// };
+
+// export const deletePatient = async (req: Request, res: Response) => {
+//   try {
+//     const id = req.params.id;
+//     const user = await db.query(`DELETE FROM patient where id = $1`, [id]);
+//     if (user.rowCount === 0) {
+//       res.status(404).json({ message: "Пациент не найден." });
+//     } else {
+//       res.status(200).json({ message: "Пациент успешно удален" });
+//     }
+//   } catch (error) {
+//     console.error("Что-то пошло не так");
+//     res
+//       .status(500)
+//       .json("Произошла ошибка при получении информации о пациенте");
+//   }
+// };
--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,7 @@ import authRoutes from "./routes/auth.route";
 import doctorRoutes from "./routes/doctor.route";
 import userRoutes from "./routes/user.route";
 import procedureRoutes from "./routes/procedure.route";
+import historyRoute from "./routes/history.route";
 
 const PORT = process.env.PORT || 8080;
 
@@ -22,5 +23,6 @@ server.use("/api", visitRoutes);
 server.use("/api", doctorRoutes);
 server.use("/api", userRoutes);
 server.use("/api", authRoutes);
+server.use("/api", historyRoute);
 
 server.listen(PORT, () => console.log(`server started on port ${PORT}`));
