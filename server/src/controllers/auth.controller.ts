d903b4e5f1cba9c8e3e151460ead4356527c6121
artemenkoS <ktkbs@ya.ru>
2023-12-23 17:41:32 +0500

    fixed

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -6,7 +6,7 @@ import { config } from '../config';
 
 const generateAccessToken = (id: string, role: string) => {
   const payload = { id: id, role: role };
-  return jwt.sign(payload, config.secretKey, { expiresIn: '10h' });
+  return jwt.sign(payload, config.secretKey, { expiresIn: '7d' });
 };
 
 export const registerUser = async (req: Request, res: Response, next: NextFunction) => {
