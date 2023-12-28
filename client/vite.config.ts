95c290dd8267838ec3eb60e731250a2969f683ba
artemenkoS <ktkbs@ya.ru>
2023-12-28 14:23:55 +0500

    rm vite legacy plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,8 @@
-import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy({
-      targets: ['Chrome <= 49'],
-    }),
-  ],
+  plugins: [react()],
   server: { port: 3000 },
 });
